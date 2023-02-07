2620c4dd8e72d3d5848bf6d9acba361fc57c79ca
mats363 <hello@matsjonsson.work>
2023-02-07 15:02:38 +0100

    wip

--- components/audio-player/AudioPlayer.tsx
+++ components/audio-player/AudioPlayer.tsx
@@ -1,7 +1,33 @@
+import { useRef, useState } from "react";
 import styles from "./AudioPlayer.module.scss";
 
-const AudioPlayer: React.FC = () => {
-  return <audio>AudioPlayer works</audio>;
+type AudioPlayerProps = {
+  audio: string;
+};
+
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ audio }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const togglePlay = () => {
+    if (audioRef.current === null) return;
+
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+
+    setIsPlaying(!isPlaying);
+  };
+  return (
+    <>
+      <audio ref={audioRef}>
+        <source src={audio!} type="audio/mpeg" />
+      </audio>
+      <button onClick={togglePlay}>{isPlaying ? "paus" : "play"}</button>
+    </>
+  );
 };
 
 export default AudioPlayer;
--- components/product-card/ProductCard.tsx
+++ components/product-card/ProductCard.tsx
@@ -1,8 +1,10 @@
 import { IProduct } from "../../models/IProduct";
 import styles from "./ProductCard.module.scss";
-import { useDispatch } from "react-redux";
 import { addToCart, removeFromCart } from "../../redux/cart.slice";
 import { useAppDispatch } from "../../hooks/ReduxHooks";
+import { useRef, useState } from "react";
+import PlayIcon from "../../assets/play.svg";
+import AudioPlayer from "../audio-player";
 
 export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const dispatch = useAppDispatch();
@@ -12,9 +14,8 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       <img src={product.product_image} alt={product.product} />
       <h2>{product.product}</h2>
       <p>{product.price}</p>
-      <audio controls>
-        <source src={product.audio_preview!} type="audio/mpeg" />
-      </audio>
+      <AudioPlayer audio={product.audio_preview} />
+
       <button onClick={() => dispatch(addToCart(product))}>Add to cart</button>
       <button onClick={() => dispatch(removeFromCart(product))}>
         Remove from cart
