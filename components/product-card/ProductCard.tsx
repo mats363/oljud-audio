c19fc5c88592312244f7d9bba4a32023c1a02b2f
mats363 <hello@matsjonsson.work>
2023-02-08 20:08:09 +0100

    works!

--- components/product-card/ProductCard.tsx
+++ components/product-card/ProductCard.tsx
@@ -1,37 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ProductCard.module.scss";
 import { useShoppingCart } from "../shopping-cart/useShoppingCart";
 import { ProductCardProps } from "./types";
 import AudioPlayer from "../audio-player";
-import ProductDetails from "../product-details";
+import Image from "next/image";
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const [show, setShow] = useState(false);
   const { addToCart, removeFromCart, cartItems } = useShoppingCart();
 
-  return (
-    <section className={styles.card}>
-      <img
-        className={styles.img}
-        src={product?.product_image!}
-        alt={product?.product!}
-      />
-      <div className={styles.audioPlayer}>
-        <AudioPlayer audio={product.audio_preview!} />
-      </div>
-      <div className={styles.info}>
-        <h2>{product?.product}</h2>
-        <p>SEK: {product?.price}:-</p>
-      </div>
-      <div>{product.description}</div>
-      <div className={styles.buttons}>
-        {cartItems.find((item) => item.id === product.id) ? (
-          <div onClick={() => removeFromCart(product.id)}>-</div>
-        ) : (
-          <div onClick={() => addToCart(product)}>+</div>
-        )}
-      </div>
-    </section>
-  );
+  useEffect(() => {
+    setShow(true);
+  }, []);
+
+  if (!show) {
+    return null;
+  } else {
+    return (
+      <section className={styles.card}>
+        {/* <img
+          className={styles.img}
+          src={product.product_image!}
+          alt={product.product!}
+        /> */}
+        <Image
+          width={300}
+          height={300}
+          className={styles.img}
+          src={product.product_image!}
+          alt={product.product!}
+        />
+        <div className={styles.audioPlayer}>
+          <AudioPlayer audio={product.audio_preview!} />
+        </div>
+        <div className={styles.title}>
+          <h2>{product.product}</h2>
+        </div>
+        <div className={styles.description}>{product.description}</div>
+        <div className={styles.info}>
+          <p>SEK: {product.price}:-</p>
+          {show && cartItems.find((item) => item.id === product.id) ? (
+            <div
+              className={styles.btn}
+              onClick={() => removeFromCart(product.id)}
+            >
+              -
+            </div>
+          ) : (
+            <div className={styles.btn} onClick={() => addToCart(product)}>
+              +
+            </div>
+          )}
+        </div>
+        <div className={styles.price}></div>
+      </section>
+    );
+  }
 };
 
 export default ProductCard;
