cec9910b8824575fb6720c1445f1186ab54313d5
mats363 <hello@matsjonsson.work>
2023-02-08 09:23:14 +0100

    wip

--- components/audio-player/AudioPlayer.tsx
+++ components/audio-player/AudioPlayer.tsx
@@ -1,7 +1,39 @@
+import { useRef, useState } from "react";
 import styles from "./AudioPlayer.module.scss";
 
-const AudioPlayer: React.FC = () => {
-  return <audio>AudioPlayer works</audio>;
+type AudioPlayerProps = {
+  audio: string;
+};
+
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ audio }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const togglePlay = () => {
+    if (audioRef.current === null) return;
+
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+
+    setIsPlaying(!isPlaying);
+  };
+  return (
+    <>
+      <audio ref={audioRef}>
+        <source src={audio!} type="audio/mpeg" />
+      </audio>
+      <div onClick={togglePlay} className={styles.togglePlay}>
+        {isPlaying ? (
+          <i className="bi bi-pause-fill"></i>
+        ) : (
+          <i className="bi bi-play"></i>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default AudioPlayer;
