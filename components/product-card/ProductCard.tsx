5638e6109b59123e8ef0fe6f5547d53ed67d8c4d
mats363 <hello@matsjonsson.work>
2023-02-08 12:12:16 +0100

    wip

--- components/product-card/ProductCard.tsx
+++ components/product-card/ProductCard.tsx
@@ -1,10 +1,10 @@
+import React from "react";
 import styles from "./ProductCard.module.scss";
 import { useShoppingCart } from "../shopping-cart/useShoppingCart";
 import { ProductCardProps } from "./types";
 import AudioPlayer from "../audio-player";
-import Image from "next/image";
 
-export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const { addToCart, removeFromCart } = useShoppingCart();
 
   return (
@@ -12,15 +12,16 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       <img
         className={styles.img}
         src={product.product_image!}
-        width={200}
-        height={200}
-        alt={product.image!}
+        alt={product.product!}
       />
+      <div className={styles.audioPlayer}>
+        {" "}
+        <AudioPlayer audio={product.audio_preview!} />
+      </div>
       <div className={styles.info}>
         <h2>{product.product}</h2>
         <p>{product.price}</p>
       </div>
-      <AudioPlayer audio={product.audio_preview!} />
       <div className={styles.buttons}>
         <button onClick={() => addToCart(product)}>Add to cart</button>
         <button onClick={() => removeFromCart(product.id)}>
